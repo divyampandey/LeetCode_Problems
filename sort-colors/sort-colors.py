0f042600ea34fcccdeed3d57def411dda6dba8ed
Divyam <divyampandey527@gmail.com>
2021-12-25 15:50:16 +0530

    Time: 24 ms (98.18%), Space: 14.3 MB (12.60%) - LeetHub

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -3,27 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-       # print(nums)
-        i = 1
-        j = 0
+        count_0 = 0
+        count_1 = 0
+        count_2 = 0
+        for i in nums:
+            if i == 0:
+                count_0 += 1
+            elif i == 1:
+                count_1 += 1
+            else:
+                count_2 += 1
         
-        while(i < len(nums)):
-            tempi = nums[i]
-            tempj = nums[j]
-           # print(tempi,tempj)
-            if tempi < tempj:
-                t = i-1
-                while(t >=0 and tempi < nums[t]):
-                    nums[t+1]=nums[t]
-                  #  print(nums)
-                 #   print('inside^^^^^^')
-                    t = t - 1
-
-                t = t + 1
-                nums[t]=tempi
-                #print(nums)
-               # print("outside-----")
-            j= j + 1
-            i= i+1
+        length = 0
+        while (length != len(nums)):
+            if count_0 > 0:
+                nums[length] = 0
+                count_0 -= 1
+                length += 1
+            elif count_1 > 0:
+                nums[length] = 1
+                count_1 -= 1
+                length += 1
+            else:
+                nums[length] = 2
+                count_2 -= 1
+                length += 1
+                
         
         
