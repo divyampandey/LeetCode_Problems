272fc06d9f8da07ec82f9b067a6b071ef8a0ce56
Divyam <divyampandey527@gmail.com>
2022-05-03 08:03:09 +0530

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.py
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.py
@@ -2,28 +2,35 @@
 #Your should return the required output
 class Solution:
     def maxLen(self, n, arr):
-        #Code here
-        max_length = 0
-        summation_dict = {}
+        # [15, -2, 2, -8, 1, 7, 10, 23]
         summation = 0
-        for i in range(n):
-            summation += arr[i]
+        max_length_of_subarray = 0
+        summation_dict = {}
+        for index,number in enumerate(arr):
+            #print(max_length_of_subarray)
+            summation += number
+            #print(summation)
             if summation == 0:
-                length = i + 1
-                if max_length < length:
-                    max_length = length
+                start_index = 0
+                end_index = index
+                len_of_subarray = (end_index - start_index)+1
+                if max_length_of_subarray < len_of_subarray:
+                    max_length_of_subarray = len_of_subarray
+            
+            if summation in summation_dict.keys():
+                start_index = summation_dict[summation] + 1
+                end_index = index
+                len_of_subarray = (end_index - start_index) + 1
+                if max_length_of_subarray < len_of_subarray:
+                    max_length_of_subarray = len_of_subarray
+                
+                #print(start_index,end_index, len_of_subarray, max_length_of_subarray)
+            
             else:
-                if summation in summation_dict.keys():
-                    start_index = summation_dict[summation] + 1
-                    end_index = i
-                    length = (end_index - start_index)+1
-                    if max_length < (length):
-                        max_length = length
-                else:
-                    summation_dict[summation] = i
-                
-        return max_length        
-                
+                summation_dict[summation] = index
+        
+        return max_length_of_subarray
+
 #{ 
 #  Driver Code Starts
 if __name__=='__main__':
