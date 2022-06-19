d8e624dafac3698bff9a593bfbd32a84d830cb08
Divyam <divyampandey527@gmail.com>
2022-06-19 15:07:25 +0530

    Time: 209 ms (82.48%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-
-select max(salary) as SecondHighestSalary from Employee where salary not in (
-select max(salary) from Employee
-    )
+select  case when max(t.rnk) > 1 then t.salary 
+             else null
+        end as SecondHighestSalary from (
+select id, salary, dense_rank()
+    over(order by salary desc) as 
+rnk
+from Employee ) t
+where t.rnk = 2
